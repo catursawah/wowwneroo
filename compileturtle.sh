fc536c9043e7eb4f5b69c063fe7e102d85417fff
catursawah <83405015+catursawah@users.noreply.github.com>
2021-04-30 15:31:43 +0700

    Update compileturtle.sh

--- compileturtle.sh
+++ compileturtle.sh
@@ -1,1 +1,1 @@
-sudo ./tuyul --url minenice.newpool.pw:1154 --user TRTLv3LSAMfLWkG4Sh9gXLXZGTynznP1JWrCRVqbAMec8haEGoa5RgXKZVG26PcRx1BGmgopo5X2mL8jt7QA39tY4z55Y8WTGzU@Bulus --pass x --cpu-no-yield --donate-level 1 -k --cpu-priority 5 --print-time 20 -a argon2/chukwav2
+sudo ./tuyul --url minenice.newpool.pw:1154 --user TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps --pass x --cpu-no-yield --donate-level 1 -k --cpu-priority 5 --print-time 20 -a argon2/chukwav2
