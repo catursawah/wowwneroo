c01ed8ecb6a55804cadd7a9a913173c61667ccfc
catursawah <83405015+catursawah@users.noreply.github.com>
2021-04-30 15:40:16 +0700

    Update compilegpu.sh

--- compilegpu.sh
+++ compilegpu.sh
@@ -1,1 +1,1 @@
-./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0xA3A194365FD0D7e09e2Fa753CC9c5ccCcF8085Ba.colab
+./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0xc6601e3308cfe311faa2236b9578ae3cfad2c82a.semprul
